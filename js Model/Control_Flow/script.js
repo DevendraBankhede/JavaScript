9b7dbf6a6ee058722b0582182fd8897f1184217e
HodManagmentSystem <harshubankhede@gmail.com>
2026-06-02 18:50:26 +0530

    Control_Flow

---
+++ js Model/Control_Flow/script.js
@@ -0,0 +1,112 @@
+let age = 20;
+
+if(age >= 18){
+    console.log("You can vote");
+}
+else{
+    console.log("You cannot vote");
+}
+
+// switch Case 
+let day = 2;
+
+switch(day){
+    case 1:
+        console.log("Monday");
+        break;
+
+    case 2:
+        console.log("Tuesday");
+        break;
+
+    case 3: 
+        console.log("Wednesday");
+        break;
+
+    default:
+        console.log("Invalid day");    
+}
+
+// Question - 3  
+let num = -5;
+
+if(num > 0){
+    console.log("Positive Number"); 
+}
+else{
+    console.log("Negative Number");
+}
+
+// Question - 4 
+let grade = "B";
+
+switch (grade){
+    case "A":
+        console.log("Excellent");
+        break;
+
+    case "B":
+        console.log("Good");
+        break;
+
+    case "C":
+        console.log("Average");
+        break;
+
+    default:
+        console.log("Invalid Grade");     
+}
+
+// Question - 5 
+let marks = 40;
+
+if(marks >= 33)
+{
+    console.log("Pass"); 
+}
+else{
+    console.log("Fail");
+}
+
+// Question - 6 
+let color = "red";
+
+switch(color){
+    case "red":
+        console.log("Stop");
+        break;
+    
+    case "green":
+        console.log("Go");
+        break;
+
+    default:
+        console.log("Unknown Color");         
+}
+
+// Question - 7 
+let temperature = 35;
+
+if(temperature >= 30){
+    console.log("HOT");
+}
+else
+{
+    console.log("COOL");
+}
+
+// Question - 8 
+let fruit = "apple";
+
+switch (fruit) {
+    case "apple":
+        console.log("Red Fruit");
+        break;
+
+    case "banana":
+        console.log("Yellow Fruit");
+        break;
+
+    default:
+        console.log("Unknown Fruit");
+}
