3ed3a4b9c5092beabc89477d154412e41219d679
HodManagmentSystem <harshubankhede@gmail.com>
2026-06-09 18:28:28 +0530

    DOM Practies

---
+++ js Model/Dom/script.js
@@ -0,0 +1,25 @@
+// let h1 = document.querySelector("h1");
+// h1.innerHTML = "Devendra";
+
+// let h1 = document.querySelector("h1");
+// h1.style.color = "red";
+// h1.style.fontFamily = "Gilroy";
+
+// let h1 = document.querySelector("h1");
+// h1.classList.add("hulu");
+// console.dir(h1);
+
+// let heading = document.querySelector("#heading");
+// heading.textContent =  "Welcome Again!!";
+
+// let btn = document.querySelector("button");
+// btn.removeAttribute("disabled");
+
+let ul = document.querySelector("ul");
+let li = document.createElement("li");
+
+li.textContent = "New Task";
+
+ul.appendChild(li);
+
+ 
