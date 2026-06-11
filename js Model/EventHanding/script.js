cf23d0d95ad6f1c8fe45ec6b61f1e2993f562887
HodManagmentSystem <harshubankhede@gmail.com>
2026-06-11 19:20:51 +0530

    change

--- js Model/EventHanding/script.js
+++ js Model/EventHanding/script.js
@@ -14,3 +14,35 @@ input.addEventListener("input",function(evt){
 });
 
 // Question -> 3
+let sel =  document.querySelector("select");
+let device = document.querySelector("#device");
+
+sel.addEventListener("change",function(evt){
+    device.textContent = `${evt.target.value} Device Selected`;
+});
+
+// Question - > 4 
+let h2 = document.querySelector("h2");
+window.addEventListener("keydown",function(dets){
+    if(dets.key === " "){
+        h2.textContent = "SPC";
+    }
+    else{
+        h2.textContent = dets.key;
+    }
+});
+
+// Question - > 5
+let btn = document.querySelector("#btn");
+let fileinp = document.querySelector("#fileinp");
+
+btn.addEventListener("click",function(){
+    fileinp .click();
+});
+
+fileinp.addEventListener("change",function(dets){
+    const file = dets.target.files[0];
+    if(file){
+        btn.textContent = file.name;
+    }
+});
