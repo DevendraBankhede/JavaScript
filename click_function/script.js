7bb506fb815a5faf533a96a48a5e0f93df5cd97d
HodManagmentSystem <harshubankhede@gmail.com>
2026-06-19 18:18:44 +0530

    click_function

---
+++ click_function/script.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    document.getElementById("text").innerHTML = "Button click ho gaya😉";
+    document.getElementById("moon").style.color = "red";
+    document.getElementById("text").style.color = "blue";
+}
